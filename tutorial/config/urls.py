4db272cc814b01551f1d3b8163579f5c4c3610d9
Booki Park <bkbkgg@gmail.com>
2017-11-06 22:25:14 +0900

    permission 추가 및 api-auth url 추가

--- tutorial/config/urls.py
+++ tutorial/config/urls.py
@@ -21,3 +21,7 @@ urlpatterns = [
     url(r'^snippets/', include('snippets.urls', namespace='snippets')),
     url(r'^users/', include('snippets.urls', namespace='users')),
 ]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
