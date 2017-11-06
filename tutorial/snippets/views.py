6557700963ef4269a26dc63ceecdd09864a4a8a9
Booki Park <bkbkgg@gmail.com>
2017-11-06 14:33:00 +0900

    comment 추가

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -11,11 +11,16 @@ from .models import Snippet
 def snippet_list(request):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
+        # 쿼리셋을 serialize 할 때는 many=True 옵션 추가
         serializer = SnippetSerializer(snippets, many=True)
+        # 기본적으로 JsonResponse는 dict형식으로 리턴하지만,
+        # safe=False이면 주어진 데이터는 dict가 아니어도 됨 (지금의 경우 리스트 객체가 옴)
         return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
+        # request로 전달 된 데이터들을 JSONParse를 사용하여 파이썬 데이터 형식으로 파싱
         data = JSONParser().parse(request)
+        # 처리 된 데이터를 사용해 SnippetSerialzier 인스턴스를 생성
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
