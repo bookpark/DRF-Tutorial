6ac40c6c6bbbac5422a2062c18793b016a51fc24
Booki Park <bkbkgg@gmail.com>
2017-11-06 14:05:23 +0900

    url 추가

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from . import views
+from django.conf.urls import url
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^snippets/', include('snippets.urls', namespace='snippets')),
+    url(r'^snippets/$', views.snippet_list, name='snippet_list'),
 ]
